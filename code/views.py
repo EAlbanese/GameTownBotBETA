2068748aecfb8decf1fa0e8f87400d1d3e2ef3bc
Elia Albanese <eliaalbanese@icloud.com>
2023-03-03 15:15:17 +0100

    ticket logs

--- code/views.py
+++ code/views.py
@@ -15,12 +15,34 @@ class variableManager(ui.View):
 class TicketManageView(ui.View):
     @ui.button(label="Ticket schliessen", style=ButtonStyle.primary)
     async def first_button_callback(self, button,  interaction: Interaction):
+        ticketlogs = await interaction.guild.fetch_channel(1076888610126696511)
+
+        ticketId = db.get_ticket_id_by_channel_id(
+            interaction.message.channel.id)
+        ticketinfo = db.get_ticket_info(ticketId)
+        ticketClosedBy = interaction.user.display_name
+        memberName = interaction.guild.get_member(ticketinfo[2])
+        moderatorName = interaction.guild.get_member(ticketinfo[3])
+
+        embed = Embed(title=f"🔒 Ticket wurde geschlossen")
+        embed.add_field(name="🎫 Ticket ID",
+                        value=f'{ticketinfo[0]}', inline=False)
+        embed.add_field(name="🎫 Channel ID",
+                        value=f'{ticketinfo[1]}', inline=False)
+        embed.add_field(name="👤 Ticket geöffnet von",
+                        value=f'{memberName}', inline=False)
+        embed.add_field(name="✅ Ticket geclaimt von",
+                        value=f'{moderatorName}', inline=False)
+        embed.add_field(name="🔒 Ticket geschlossen von",
+                        value=f'{ticketClosedBy}', inline=False)
+
+        await ticketlogs.send(embed=embed)
         await interaction.response.pong()
         await interaction.channel.delete()
 
     @ui.button(label="Claim Ticket", style=ButtonStyle.primary)
     async def second_button_callback(self, button, interaction: Interaction):
-        staffrole = interaction.guild.get_role(1072489048515559506)
+        staffrole = interaction.guild.get_role(1081209944545689730)
         if staffrole not in interaction.user.roles:
             await interaction.response.send_message("⛔ Keine Berechtigung!", ephemeral=True)
             return
@@ -29,6 +51,26 @@ class TicketManageView(ui.View):
         embed.author.name = interaction.user.display_name
         embed.author.icon_url = interaction.user.display_avatar
         await interaction.response.send_message(embed=embed)
+
+
+# class TicketLogsView(ui.View):
+#     @ui.button(label="🔓 Ticket erneut öffnen", style=ButtonStyle.primary)
+#     async def reopenTicket(self, button,  interaction: Interaction):
+
+#         channel = interaction.guild.get_channel(
+#             int(interaction.message.embeds[0].fields[1].value))
+
+#         print(channel)
+
+#         reopenticket = await interaction.guild.create_text_channel(f"{memberName} - {ticketId}", category=category, overwrites={
+#             interaction.user: PermissionOverwrite(read_messages=True),
+#             interaction.guild.default_role: PermissionOverwrite(
+#                 read_messages=False),
+#             staffrole: PermissionOverwrite(read_messages=True)
+#         })
+
+#         await thread.edit(archived=False, locked=False)
+#         await interaction.response.send_message(f"<#{thread.id}> Ticket wurde wieder geöffnet", ephemeral=True)
 
 
 class SupportModal(ui.Modal):
@@ -43,14 +85,14 @@ class SupportModal(ui.Modal):
             title="Anliegen", description="✅ Danke, dass du dich an den Support gewandt hast. Unser Team wird sich gut darum kümmern!")
         embed.add_field(name="Wo benötigst du Hilfe?",
                         value=self.children[0].value)
-        category = await interaction.guild.fetch_channel(1075698931205427262)
+        category = await interaction.guild.fetch_channel(1070626850042298378)
 
         create_date = datetime.datetime.now()
         db.create_ticket(interaction.user.id,
                          round(create_date.timestamp()))
         count = db.get_ticket_id(round(create_date.timestamp()))
 
-        staffrole = interaction.guild.get_role(1072489048515559506)
+        staffrole = interaction.guild.get_role(1081209944545689730)
         ticketchannel = await interaction.guild.create_text_channel(f"{interaction.user.display_name} - {count}", category=category, overwrites={
             interaction.user: PermissionOverwrite(read_messages=True),
             interaction.guild.default_role: PermissionOverwrite(
@@ -58,7 +100,7 @@ class SupportModal(ui.Modal):
             staffrole: PermissionOverwrite(read_messages=True)
         })
 
-        db.update_ticket(variableManager.threadID, count)
+        db.update_ticket(ticketchannel.id, count)
 
         await interaction.response.send_message(f"Ticket eröffnet in <#{ticketchannel.id}>", ephemeral=True)
         await ticketchannel.send(f"<@{interaction.user.id}> <@&{staffrole.id}>", embed=embed, view=TicketManageView())
