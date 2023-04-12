6bb40632ac5208ae1542848282cd0c56047d155a
Elia Albanese <eliaalbanese@icloud.com>
2023-04-12 10:11:30 +0200

    kein plan

--- code/botvoiceview.py
+++ code/botvoiceview.py
@@ -3,21 +3,11 @@ from discord import ui, ButtonStyle, InputTextStyle, Interaction, Embed, Permiss
 
 class variableManager(ui.View):
     voice_channel = 0
-    voice_member = 0
-    user_count = 0
+    voice_member = []
+    user_count = []
 
 
 # Buttons
-class KickSelectDropdownView(ui.View):
-    def __init__(self):
-        super().__init__(timeout=None)
-
-    @ui.select(placeholder="Einstellungen für deinen Voice Channel",
-               min_values=1,
-               max_values=1,
-               options=[
-                   SelectOption(f"{i+1} user{'s' if i != 0 else ''} in VC", value=f"{i+1}") for i in range(user_count)
-               ])
 class ChannelSettingsButtonView(ui.View):
     def __init__(self):
         super().__init__(timeout=None)
@@ -53,13 +43,15 @@ class ChannelSettingsButtonView(ui.View):
         await interaction.response.send_modal(EditModal(title="Umbenennen"))
 
     @ ui.button(emoji="🦶", style=ButtonStyle.gray)
-    async def kick_button_callback(self, button, interaction):
-        await interaction.response.send_modal(KickModal(title="Kick"))
+    async def kick_button_callback(self, button, interaction: Interaction):
+        embed = Embed(
+            title=f'Member kicken',
+            description='Wähle mindestens einen Member, welchen du aus dem Channel kicken möchtest.',
+        )
 
-        # variableManager.voice_channel = interaction.user.voice.channel
-        # variableManager.voice_member = variableManager.voice_channel.members
-        # variableManager.user_count = len(variableManager.voice_member)
+        await interaction.channel.send(embed=embed, view=KickMemberView())
 
+        # await interaction.response.send_modal(KickModal(title="Kick"))
         # @ui.select(
         #     placeholder="Wähle mind. einen User",
         #     min_values=1,
@@ -77,23 +69,39 @@ class KickMemberView(ui.View):
     def __init__(self, *args, **kwargs) -> None:
         super().__init__(*args, **kwargs, timeout=None)
 
-    @ui.select(
-        placeholder="Wähle mind. einen User",
-        min_values=1,
-        max_values=len(variableManager.user_count),
-        options=[
-            SelectOption(f"{i+1} user{'s' if i != 0 else ''} in VC", value=f"{i+1}") for i in range(variableManager.user_count)
-        ]
-    )
-    async def select_callback(self, select, interaction: Interaction):
-        voicechannel = interaction.user.voice.channel
-        for member in voicechannel.members:
-            if member.name == self.children[0].value:
-                await member.move_to(None)
-        embed = Embed(
-            title=f"✅ {self.children[0].value} wurde aus dem Channel gekickt.")
+    async def kickmembers(interaction: Interaction):
+        # Get the voice channel the user is in
+        voice_channel = interaction.user.voice.channel
 
-        await interaction.response.send_message(embed=embed, ephemeral=True)
+        # Get all members in the voice channel
+        members = voice_channel.members
+
+        # Create the select dropdown with options for each member
+        dropdown = SelectOption(
+            placeholder="Wähle mind. einen Member aus",
+            min_values=1,
+            max_values=len(members),
+            options=[
+                SelectOption(
+                    label=member.display_name,
+                    value=str(member.id)
+                ) for member in members
+            ]
+        )
+
+        # Send the select dropdown to the user
+        message = await interaction.channel.send("Kick members from voice channel:", view=dropdown)
+
+        # Define a callback function for when the user clicks the "Kick" button
+        async def callback(interaction):
+            selected_members = [member for member in members if str(
+                member.id) in interaction.values]
+            for member in selected_members:
+                await member.move_to(None)
+            await interaction.response.edit_message(content="Selected members have been kicked from the voice channel.")
+
+        # Add the callback function to the select dropdown
+        dropdown.callback = callback
 
 
 # Modal
